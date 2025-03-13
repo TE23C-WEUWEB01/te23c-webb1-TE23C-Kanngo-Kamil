5c31eb2de7346381163ac094b6dca1451ed02505
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-03-13 14:26:15 +0100

    Tränat på Javascript, variabler, konkatenering

---
+++ kapiter-8/intro-js/script.js
@@ -0,0 +1,9 @@
+var förnamn = "Kamil ";
+var efternamn = "Adilbish";
+var ålder = 18
+var namn = förnamn + efternamn;
+var hobby = "musik"
+var kostnad = 1428
+var meddelande = namn + " är " + ålder + " år gammal och gillar " + hobby + " som kostar " + kostnad + " kr per år.";
+// 
+alert(meddelande);
