17d0ee985e78fb12c07a6762fb793e8f743f846d
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-03-21 09:43:58 +0100

    Tränat på if-satser och loopar

---
+++ kapiter-8/grunder-js/script.js
@@ -0,0 +1,23 @@
+// var tal1 = parseInt(prompt("Skriv in ett heltal:"));
+// var tal2 = parseInt(prompt("Skriv in ett till heltal:"));
+
+// if (tal1 > tal2) {
+//     document.write(`${tal1} är större än ${tal2}`);
+// } else {
+//     document.write(`${tal2} är större än eller lika med ${tal1}`);
+// }
+
+// while (true) {
+//     var konto = prompt(("Skriv in ditt kontonamn:"));
+//     var lösenord = prompt(("Skriv in ditt lösenord:"));
+//     if (lösenord == "kamil123") {
+//         document.write(`Du, ${konto}, är inloggad.`);
+//         break;
+//     } else {
+//         document.write(`Du, ${konto}, har skrivit fel lösenord.`);
+//     }
+// }
+
+for (let index = 20; index < 31; index++) {
+    document.write(`tal: ${index} <br>`);
+}
