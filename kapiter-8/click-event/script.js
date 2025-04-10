20570186daf3b3521bff1ad7488a8064cf9b1fec
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-04-10 14:27:16 +0200

    Tränat på click-event, ändra stil, och dölja/visa text och bild

---
+++ kapiter-8/click-event/script.js
@@ -0,0 +1,21 @@
+function setDarkMode() {
+    document.body.style = "background: #000;color: #fff";
+}
+function setLightMode() {
+    document.body.style = "background: #fff;color: #000";
+}
+function setFunnyMode() {
+    document.querySelector("h1").style = "font-size:100px;background: #ff0000;color: #f2ff00;";
+}
+function hide() {
+    document.querySelector("h1").style = "visibility: hidden;";
+}
+function show() {
+    document.querySelector("h1").style = "visibility: visible;";
+}
+function lyrics() {
+    document.querySelector("p").style = "visibility: visible;";
+}
+function image() {
+    document.querySelector("img").style = "visibility: visible;";
+}
